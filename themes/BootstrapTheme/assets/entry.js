4a60c2d6a290f0dc8b4e93f3ed44cbd0677dd2b0
Youri Westerman <youri.westerman@nedac.eu>
2020-07-01 12:17:36 +0200

    Installed bootstrap theme

---
+++ themes/BootstrapTheme/assets/entry.js
@@ -0,0 +1,5 @@
+import '../../../../../vendor/sylius/bootstrap-theme/assets/js/index';
+import './scss/index.scss';
+import '../../../../../vendor/sylius/bootstrap-theme/assets/media/sylius-logo.png';
+import '../../../../../vendor/sylius/bootstrap-theme/assets/js/fontawesome';
+
